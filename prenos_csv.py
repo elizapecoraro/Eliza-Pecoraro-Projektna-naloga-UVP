c2dba77dab515002acdc92db7b6cb16e3f816a41
elizapecoraro <eliza.pecorar@gmail.com>
2024-08-20 23:20:05 +0200

    dodajanje

--- prenos_csv.py
+++ prenos_csv.py
@@ -18,3 +18,20 @@ Tabela_na_spletni_strani = pregled_kode.find('table')
 #Definiramo dva seznama v katere bomo  shranili podatke
 glava = []
 vrstice = []
+
+#Poiščemo vrstice v tabeli
+vrstice_na_vrhu = Tabela_na_spletni_strani.find('thead').find('tr')
+vnosi = [th.get_text(strip=True) for th in vrstice_na_vrhu.find_all('th') if th.get_text(strip=True)]
+
+#Pregledamo vse vrednosti v tabeli in jih shranimo v seznam
+for vrstica in Tabela_na_spletni_strani.find('tbody').find_all('tr'):
+        stolpci = vrstica.find_all('td')
+        podatki = [stolp.get_text(strip=True) for stolp in stolpci]
+        vrstice.append(podatki)
+
+#Nakoncu definiramo CVS datoteko in shranimo podatke
+izpis_podatkov = 'nba.csv'
+with open(izpis_podatkov, mode='w', newline='', encoding='utf-8') as file:
+        zapis = csv.writer(file)
+        zapis.writerow(glava) 
+        zapis.writerows(vrstice)
